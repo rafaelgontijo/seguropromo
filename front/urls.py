e442fdc5feab47128db0b6c24629c6b5478ce845
Rafael Gontijo <rafael@rafaelgontijo.com>
2017-10-17 14:27:55 -0200

    scren of quotation

--- front/urls.py
+++ front/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from front.views import Home
+from front.views import HomeView, QuotationView
 
 urlpatterns = [
-    url(r'^', Home.as_view(), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^pesquisa/(?P<slug>[\w-]+)/(?P<begin_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})$',
+        QuotationView.as_view(), name='quotation'),
 ]
--- front/views.py
+++ front/views.py
@@ -1,10 +1,17 @@
+import json
+from datetime import datetime
+
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
+
+from api.models import Products, Quotation
 from front.forms import FormQuotation
+from front.utils import json2obj
 
 
-class Home(View):
+class HomeView(View):
     form_class = FormQuotation
     template_name = 'front/home.html'
 
@@ -15,6 +22,68 @@ class Home(View):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect(reverse(
+                'front:quotation',
+                kwargs={
+                    'slug': form.cleaned_data.get('destiny'),
+                    'begin_date': "{:%Y-%m-%d}".format(
+                        form.cleaned_data.get('begin_date')),
+                    'end_date': "{:%Y-%m-%d}".format(
+                        form.cleaned_data.get('end_date')),
+                }
+            ))
+
+        return render(request, self.template_name, {'form': form})
+
+
+class QuotationView(View):
+    form_class = FormQuotation
+    template_name = 'front/quotation.html'
+
+    def get(self, request, *args, **kwargs):
+        begin_date = datetime.strptime(
+            kwargs.get('begin_date'), '%Y-%m-%d')
+        end_date = datetime.strptime(
+            kwargs.get('end_date'), '%Y-%m-%d')
+        form = self.form_class(initial={
+            'destiny': kwargs.get('slug'),
+            'begin_date': begin_date,
+            'end_date': end_date,
+        })
+        quotations = Quotation()
+        products = Products()
+        quotations = quotations.calculate(
+            destination=kwargs.get('slug'),
+            begin_date=begin_date,
+            end_date=end_date
+        )
+        for quotation in quotations:
+            product = products.list(quotation.get('code'))
+            hospital_care = [e for e in product.get(
+                'benefits') if e['code'] == '50'][0].get('coverage')
+            luggage_insurance = [e for e in product.get(
+                'benefits') if e['code'] == '42'][0].get('coverage')
+            quotation['hospital_care'] = hospital_care
+            quotation['luggage_insurance'] = luggage_insurance
+
+        quotations = json.dumps(quotations)
+        quotations = json2obj(quotations)
+        return render(
+            request, self.template_name,
+            {'form': form, 'quotations': quotations})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse(
+                'front:quotation',
+                kwargs={
+                    'slug': form.cleaned_data.get('destiny'),
+                    'begin_date': "{:%Y-%m-%d}".format(
+                        form.cleaned_data.get('begin_date')),
+                    'end_date': "{:%Y-%m-%d}".format(
+                        form.cleaned_data.get('end_date')),
+                }
+            ))
 
         return render(request, self.template_name, {'form': form})
