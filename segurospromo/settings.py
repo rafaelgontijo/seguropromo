43216e5dc01d072b6b7db4a7d4dea455810866ea
Rafael Gontijo <rafael@rafaelgontijo.com>
2017-10-17 14:28:55 -0200

    pep 8 in settiogs.py

--- segurospromo/settings.py
+++ segurospromo/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-import os, environ
+import environ
+import os
 env = environ.Env(DEBUG=(bool, False),)
 environ.Env.read_env()
 
